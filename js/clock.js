e5528fd7dcbfdfc98805081fda8f2f74e9b41fcb
Dainyleen Masangkay <dainyleen@gmail.com>
2021-07-02 12:12:58 +1200

    Added getTime function and passed in a callback

---
+++ js/clock.js
@@ -0,0 +1,11 @@
+const clockSection = document.getElementById('clock')
+
+function getTime() {
+  function pad(number) {
+    if (number < 10) {
+      return "0" + number
+    } else {
+      return number
+    }
+  }
+}
